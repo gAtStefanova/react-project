f9bebfb4c19e816fe74c35fc462c28100a1e4ebd
gAtStefanova <geri_atanasova_@abv.bg>
2023-04-03 11:55:23 +0300

    Home-Page

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,17 @@
+export const Home = () => {
+    return (
+        <main className="page">
+            <header className="home">
+                <div className="home-container">
+                    <div className="home-text">
+                        <h1>Cook with me!</h1>
+                        <h4>easy and fast recipes for everyone!</h4>
+                    </div>
+                </div>
+            </header>
+            <section className="recipes-container">
+            </section>
+        </main>
+    );
+}
+
